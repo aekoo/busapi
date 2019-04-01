07b972e1babf33fe26cccf310dda70e8bd9f9b4c
aekoo <tanyecheng@yofish.com>
2019-04-01 17:31:21 +0800

    https

--- index-old.js
+++ index-old.js
@@ -1,15 +1,6 @@
 const express = require('express');
-const fs = require('fs')
 const http = require('http');
-const https = require('https');
 const app = express();
-
-var privateKey = fs.readFileSync('./certificate/private.key', 'utf8');
-var certificate = fs.readFileSync('./certificate/certificate.pem', 'utf8');
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-}
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*') // 第二个参数表示允许跨域的域名，* 代表所有域名
@@ -42,17 +33,17 @@ app.get('/api/getline', (req, res) => {
     , a = "TYMON_" + b;
   http.get({
     hostname: '182.254.143.172',
-    path: '/fkgis-gateway/' + a + '/gis/keyquery.json?' + param
+    path: '/fkgis-gateway/' + a + '/gis/keyquery.json?' +param
   }, function (data) {
-    var body = [];
-    data.on('data', function (chunk) {
-      body.push(chunk);
+      var body = [];
+      data.on('data', function (chunk) {
+        body.push(chunk);
+      });
+      data.on('end', function () {
+        body = Buffer.concat(body);
+        res.send(body.toString())
+      });
     });
-    data.on('end', function () {
-      body = Buffer.concat(body);
-      res.send(body.toString())
-    });
-  });
 });
 
 //获取站点信息
@@ -61,21 +52,17 @@ app.get('/api/carmonitor', (req, res) => {
   let param = req.url.split('?')[1] || '';
   http.get({
     hostname: 'www.jtcx.sh.cn',
-    path: '/dynamictraffic_interface/web/trafficline/carmonitor?' + param
+    path: '/dynamictraffic_interface/web/trafficline/carmonitor?' +param
   }, function (data) {
-    var body = [];
-    data.on('data', function (chunk) {
-      body.push(chunk);
+      var body = [];
+      data.on('data', function (chunk) {
+        body.push(chunk);
+      });
+      data.on('end', function () {
+        body = Buffer.concat(body);
+        res.send(body.toString())
+      });
     });
-    data.on('end', function () {
-      body = Buffer.concat(body);
-      res.send(body.toString())
-    });
-  });
 });
 
-// 创建https服务器实例
-const httpsServer = https.createServer(credentials, app)
-// 启动服务器，监听对应的端口
 app.listen(80, () => console.log('开始监听80端口!'));
-httpsServer.listen(443, () => { console.log(`开始监听443端口!`) })
