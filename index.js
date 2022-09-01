e2b047f4e9c14e401c45240fda80f99952337794
aekoo <717750121@qq.com>
2022-09-01 23:41:34 +0800

    接口优化，v1.0.5

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const credentials = {
   key: privateKey,
   cert: certificate,
 }
+const error_result = { code: 500, data: [], message: '噢~出错了，程序员又要加班了！' };
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*') // 第二个参数表示允许跨域的域名，* 代表所有域名
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS') // 允许的 http 请求的方法
+  // 允许前台获得的除 Cache-Control、Content-Language、Content-Type、Expires、Last-Modified、Pragma 这几张基本响应头之外的响应头
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+  if (req.method == 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
+app.use('/bus', express.static('bushere'))
+
+
 
 const getLineBase = (lineName) => {
   var regExp = /^\d+(\.\d+)?$/;
@@ -74,141 +91,63 @@ const getArriveBase = (line_id, lineName, direction, stopid) => {
   });
 }
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') // 第二个参数表示允许跨域的域名，* 代表所有域名
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS') // 允许的 http 请求的方法
-  // 允许前台获得的除 Cache-Control、Content-Language、Content-Type、Expires、Last-Modified、Pragma 这几张基本响应头之外的响应头
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-  if (req.method == 'OPTIONS') {
-    res.sendStatus(200)
-  } else {
-    next()
-  }
-})
-
-app.use('/bus', express.static('bushere'))
-
 //获取线路
-app.get('/api/getline', (req, res) => {
-  var path = req.originalUrl;
-  let param = req.url.split('?')[1] || '';
-  var b = new Date
-    , a = b.getMonth() + 1 + ""
-    , d = b.getDate() + ""
-    , c = b.getHours() + ""
-    , e = b.getMinutes() + ""
-    , b = b.getFullYear() + ""
-    , b = b + (1 == a.length ? "0" + a : a)
-    , b = b + (1 == d.length ? "0" + d : d)
-    , b = b + (1 == c.length ? "0" + c : c)
-    , b = b + (1 == e.length ? "0" + e : e)
-    , a = "TYMON_" + b;
-  http.get({
-    hostname: 'www.trafficeye.com.cn',
-    path: '/fkgis-gateway/' + a + '/gis/keyquery.json?' + param
-  }, function (data) {
-    var body = [];
-    data.on('data', function (chunk) {
-      body.push(chunk);
-    });
-    data.on('end', function () {
-      body = Buffer.concat(body);
-      res.send(body.toString())
-    });
-  });
-});
-
-//获取线路
-// app.get('/api/getline', (req, res) => {
-//   var path = req.originalUrl;
-//   let { lineName } = req.query;
-//   console.log(req.query);
-//   let regExp = /^\d+(\.\d+)?$/;
-//   if (regExp.test(lineName)) {
-//     lineName = lineName + '路';
-//   }
-//   http.get({
-//     hostname: 'apps.eshimin.com',
-//     path: '/traffic/gjc/getBusBase?name=' + encodeURI(lineName)
-//   }, function (data) {
-//     var body = [];
-//     data.on('data', function (chunk) {
-//       console.log('chunk', chunk);
-//       body.push(chunk);
-//     });
-//     data.on('end', function () {
-//       body = Buffer.concat(body);
-//       console.log('body', body.toString());
-//       res.send(body.toString())
-//     });
-//   });
-// });
-
-//获取站点信息
-// app.get('/api/carmonitor', (req, res) => {
-//   var path = req.originalUrl;
-//   let param = req.url.split('?')[1] || '';
-//   http.get({
-//     hostname: 'www.jtcx.sh.cn',
-//     path: '/dynamictraffic_interface/web/trafficline/carmonitor?' + param
-//   }, function (data) {
-//     var body = [];
-//     data.on('data', function (chunk) {
-//       body.push(chunk);
-//     });
-//     data.on('end', function () {
-//       body = Buffer.concat(body);
-//       res.send(body.toString())
-//     });
-//   });
-// });
-
-
-//获取站点信息
-app.get('/api/carmonitor', async (req, res) => {
+app.get('/api/getline', async (req, res) => {
   var path = req.originalUrl;
   let param = querystring.parse(req.url.split('?')[1] || '');
+  let { cityCode, lineName } = param;
 
-  let lineName = param.busname;
   try {
-
     const resLinebase = await getLineBase(lineName);
     const { line_id, line_name, start_stop, start_earlytime, start_latetime, end_stop, end_earlytime, end_latetime } = JSON.parse(resLinebase);
-    // console.log(line_id);
+    if (!line_id) return res.send({ code: -1, datas: {}, message: '没有查询到该公交线路' });
     const lineResponse = await getBusStop(line_id, lineName);
     // console.log(JSON.parse(lineResponse));
-    const direction = param.startstop == start_stop ? 0 : 1;
-    const stops = JSON.parse(lineResponse)[`lineResults${direction}`].stops || [];
+    const stops0 = JSON.parse(lineResponse)[`lineResults0`].stops || [];
+    const stops1 = JSON.parse(lineResponse)[`lineResults1`].stops || [];
 
-    let stopids = stops.filter((val) => {
-      if (val.zdmc == param.stopname) {
-        return val.id;
-      } else {
-        return 0;
-      }
-    });
-    
-    if (stopids.length) {
-      let stopid = stopids[0].id
-
-      const getResponse = await getArriveBase(line_id, lineName, direction, stopid);
-      // console.log(JSON.parse(getResponse));
-      let B = getResponse.replace('cars', 'realtime')
-      B = B.replace('[', '')
-      B = B.replace(']', '')
-      if (B == "{ }") {
-        B = { "result": {} };
-      } else {
-        B = { "result": JSON.parse(B) }
-        console.log(B);
-      }
-      res.send(B);
-    } else {
-      res.send({ "result": {} });
+    let result_data = {
+      line_id,// 线路id
+      line_name,// 线路名称
+      start_stop,// 起点站
+      end_stop,// 终点站
+      start_earlytime,// 起点站首班车时间
+      start_latetime,// 终点站末班车时间
+      end_earlytime,// 反向起点站首班车时间
+      end_latetime,// 反向终点站末班车时间
+      line_stops: { stops0, stops1 },// 站点列表
     }
+    res.send({ code: 1, datas: result_data, message: '成功' });
 
   } catch (error) {
     console.log(error);
+    res.send(error_result);
+  }
+});
+
+// 获取站点信息
+app.get('/api/carmonitor', async (req, res) => {
+  var path = req.originalUrl;
+  let param = querystring.parse(req.url.split('?')[1] || '');
+  let { lineId, lineName, direction, stopid } = param;
+
+  try {
+    const getResponse = await getArriveBase(lineId, lineName, direction, stopid);
+    // console.log(JSON.parse(getResponse));
+    let B = getResponse.replace('cars', 'realtime')
+    B = B.replace('[', '')
+    B = B.replace(']', '')
+    if (B == "{ }") {
+      B = {};
+    } else {
+      B = { ...JSON.parse(B) }
+      // console.log(B);
+    }
+    res.send({ code: 1, datas: B, message: '成功' });
+
+  } catch (error) {
+    console.log(error);
+    res.send(error_result);
   }
 });
 
